5bc33a4e2356c95417bdb3ecdb431451313301d6
tomas-kramaric <tomas.kramaric@sk.ibm.com>
2017-01-07 17:35:43 +0100

    fix: color echo on mac

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -7,36 +7,36 @@
 # Taken from Color Bash Prompt HowTo).
 
 # Normal Colors
-Black='\e[0;30m'        # Black
-Red='\e[0;31m'          # Red
-Green='\e[0;32m'        # Green
-Yellow='\e[0;33m'       # Yellow
-Blue='\e[0;34m'         # Blue
-Purple='\e[0;35m'       # Purple
-Cyan='\e[0;36m'         # Cyan
-White='\e[0;37m'        # White
+Black='\x1B[0;30m'        # Black
+Red='\x1B[0;31m'          # Red
+Green='\x1B[0;32m'        # Green
+Yellow='\x1B[0;33m'       # Yellow
+Blue='\x1B[0;34m'         # Blue
+Purple='\x1B[0;35m'       # Purple
+Cyan='\x1B[0;36m'         # Cyan
+White='\x1B[0;37m'        # White
 
 # Bold
-BBlack='\e[1;30m'       # Black
-BRed='\e[1;31m'         # Red
-BGreen='\e[1;32m'       # Green
-BYellow='\e[1;33m'      # Yellow
-BBlue='\e[1;34m'        # Blue
-BPurple='\e[1;35m'      # Purple
-BCyan='\e[1;36m'        # Cyan
-BWhite='\e[1;37m'       # White
+BBlack='\x1B[1;30m'       # Black
+BRed='\x1B[1;31m'         # Red
+BGreen='\x1B[1;32m'       # Green
+BYellow='\x1B[1;33m'      # Yellow
+BBlue='\x1B[1;34m'        # Blue
+BPurple='\x1B[1;35m'      # Purple
+BCyan='\x1B[1;36m'        # Cyan
+BWhite='\x1B[1;37m'       # White
 
 # Background
-On_Black='\e[40m'       # Black
-On_Red='\e[41m'         # Red
-On_Green='\e[42m'       # Green
-On_Yellow='\e[43m'      # Yellow
-On_Blue='\e[44m'        # Blue
-On_Purple='\e[45m'      # Purple
-On_Cyan='\e[46m'        # Cyan
-On_White='\e[47m'       # White
+On_Black='\x1B[40m'       # Black
+On_Red='\x1B[41m'         # Red
+On_Green='\x1B[42m'       # Green
+On_Yellow='\x1B[43m'      # Yellow
+On_Blue='\x1B[44m'        # Blue
+On_Purple='\x1B[45m'      # Purple
+On_Cyan='\x1B[46m'        # Cyan
+On_White='\x1B[47m'       # White
 
-NC="\e[m"               # Color Reset
+NC="\x1B[m"               # Color Reset
 
 function getIP() {
     echo "`ip route get 8.8.8.8 | awk '{print $NF; exit}'`"
